80d9f435d31d75fe007e1bc2123ebbf4aa87a31f
Jacob Monnikendam <jacobmonn@sbcglobal.net>
2017-06-05 22:30:17 -0500

    adds test code for interval timing

--- Scroller/Scroller.js
+++ Scroller/Scroller.js
@@ -1,7 +1,24 @@
-class Scroller {
-	constructor() {
-		this.speed;
-	}
+// class Scroller {
+// 	constructor() {
+// 		this.speed;
+// 		this.
+// 	}
+// }
 
-	
+var $startText = $('#start-text'),
+	$scrolling = $('#scrolling');
+
+function scroll(str) {
+	(function interval(i) {
+		console.log(str[i]);
+		i++;
+		if (i < str.length) {
+			setTimeout(function () { interval(i); }, 500);
+		}
+	})(0);
 }
+
+$(document).click(function () {
+	$startText.css('display', 'none');
+
+});
