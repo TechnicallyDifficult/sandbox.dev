53e1fd4ae00ebbffed72e9630a32102a3cf182ee
Jacob Monnikendam <jacobmonn@sbcglobal.net>
2017-06-06 02:08:14 -0500

    most everything works now

--- Scroller/Scroller.js
+++ Scroller/Scroller.js
@@ -6,19 +6,76 @@
 // }
 
 var $startText = $('#start-text'),
-	$scrolling = $('#scrolling');
+	$elements = $('.scrolling'),
+	speed = 32,
+	currentSpeed = speed,
+	scrolling = false,
+	speedUpKeys = ['x', 'escape', 'control'],
+	keydown = null,
+	nextKeys = ['z', ' ', 'enter'];
 
-function scroll(str) {
+function scroll(str, $element) {
+	var deferred = $.Deferred();
+
+	scrolling = true;
+
 	(function interval(i) {
-		$scrolling.append(str[i]);
+		$element.append(str[i]);
 		i++;
 		if (i < str.length) {
-			setTimeout(function () { interval(i); }, 200);
+			setTimeout(() => interval(i), currentSpeed);
+		} else {
+			setTimeout(() => scrolling = false , speed * 2);
+			deferred.resolve();
 		}
 	})(0);
+
+	return deferred.promise();
 }
 
-$(document).click(function () {
+function next() {
 	$startText.css('display', 'none');
-	scroll('This text scrolls onscreen.');
-});
+
+	if (scrolling) return;
+
+	return scroll('This text scrolls onscreen.', $elements.filter(':empty:first'));
+}
+
+function speedUp() {
+	currentSpeed /= 2;
+}
+
+function speedDown() {
+	currentSpeed *= 2;
+}
+
+$(document).click(next)
+	.contextmenu((e) => {
+		e.preventDefault();
+	}).mousedown((e) => {
+		if (e.which === 3 && !keydown) {
+			keydown = e.which;
+			e.preventDefault();
+			speedUp();
+		}
+	}).mouseup((e) => {
+		if (e.which === 3 && e.which === keydown) {
+			e.preventDefault();
+			speedDown();
+			keydown = null;
+		}
+	}).keydown((e) => {
+		if ($.inArray(e.key.toLowerCase(), speedUpKeys) !== -1 && !keydown) {
+			keydown = e.key;
+			speedUp();
+		}
+	}).keyup((e) => {
+		if ($.inArray(e.key.toLowerCase(), speedUpKeys) !== -1 && e.key.toLowerCase() === keydown) {
+			speedDown();
+			keydown = null;
+		}
+	}).keypress((e) => {
+		if ($.inArray(e.key.toLowerCase(), nextKeys) !== -1) {
+			next();
+		}
+	});
